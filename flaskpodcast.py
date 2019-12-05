3950793635e33d9a36b9b643f1fbcefbaecf5d52
Manuel Fernandez <ferna285@umn.edu>
2019-12-05 14:42:02 -0600

    navbar

--- flaskpodcast.py
+++ flaskpodcast.py
@@ -1,34 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-
-
-class Podcast(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-    image = db.Column(db.String(500), nullable=False)
-    link = db.Column(db.String(300))
-
-class Episode(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(300), nullable=False)
-    link = db.Column(db.String(500), nullable=False)
-    audio_url = db.Column(db.String(500), nullable=False, unique=True)
-    time_published = db.Column(db.Integer, nullable=False)
-    length = db.Column(db.Integer, nullable=False)
+# Bootstrap(app)
 
 podcasts = [
     {
@@ -51,3 +25,47 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+
+@app.route("/test")
+def test():
+    return render_template('test.html', title="Test")
+
+@app.route('/starter')
+def starter():
+    return render_template('starter.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# db = SQLAlchemy(app)
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), unique=True, nullable=False)
+#     email = db.Column(db.String(100), unique=True, nullable=False)
+#     password = db.Column(db.String(60), nullable=False)
+
+
+# class Podcast(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100), nullable=False)
+#     description = db.Column(db.String(500), nullable=False)
+#     image = db.Column(db.String(500), nullable=False)
+#     link = db.Column(db.String(300))
+
+# class Episode(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(300), nullable=False)
+#     link = db.Column(db.String(500), nullable=False)
+#     audio_url = db.Column(db.String(500), nullable=False, unique=True)
+#     time_published = db.Column(db.Integer, nullable=False)
+#     length = db.Column(db.Integer, nullable=False)
+
+
+
+
