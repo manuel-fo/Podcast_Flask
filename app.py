f1e5fc68fad155a4e3624810a9f11b274b71d60f
manuel-fo <manuel.oromendia@gmail.com>
2019-12-16 15:59:14 -0600

    change pass length

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(600), nullable=False)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
